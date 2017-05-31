d0d3a17b32095f06be02df6c2bd3db5d955d4cfb
Bowen Yao <vinyao1994@gmail.com>
2017-05-31 13:41:16 -0700

    .

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python AGE_32.py --dataset imagenet --batch_size 64 --nepoch 1 --drop_lr 3 --c_dim 3 --z_dim 128 --miu 10 --lamb 2000 --g_step 2
+python AGE_32.py --dataset imagenet --batch_size 64 --nepoch 6 --drop_lr 3 --c_dim 3 --z_dim 128 --miu 10 --lamb 2000 --g_step 2
 
-python AGE_32.py --dataset imagenet --batch_size 256 --nepoch 1 --drop_lr 3 --c_dim 3 --z_dim 128 --miu 10 --lamb 2000 --g_step 2 --restore True
+python AGE_32.py --dataset imagenet --batch_size 256 --nepoch 6 --drop_lr 3 --c_dim 3 --z_dim 128 --miu 10 --lamb 2000 --g_step 2 --restore True
